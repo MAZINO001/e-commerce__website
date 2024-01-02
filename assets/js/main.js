3cc1dc5f41c8779d30a701d3dcf5724455ea554f
mounir <marnissimounir05@gmail.com>
2024-01-02 17:47:01 +0100

    add-swier

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,12 @@
 /*=============== IMAGE GALLERY ===============*/
 
 /*=============== SWIPER CATEGORIES ===============*/
-
+var swiper = new Swiper(".categories__container", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== PRODUCTS TABS ===============*/
