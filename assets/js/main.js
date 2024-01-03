2743c2beb0f900050371748c3ca3eaada532fbc6
mounir <marnissimounir05@gmail.com>
2024-01-03 16:38:44 +0100

    add NEW ARRIVALS & DEALS

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@
 var swiperCategories = new Swiper(".categories__container", {
     spaceBetween: 24,
     loop: true,
-    
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -37,5 +37,49 @@ var swiperCategories = new Swiper(".categories__container", {
     },
 });
 /*=============== SWIPER PRODUCTS ===============*/
+var swiperProducts = new Swiper(".new__container", {
+    spaceBetween: 24,
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+});
 
 /*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+    tabsContents = document.querySelectorAll("[content]")
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target)
+        tabsContents.forEach((tabsContent) => {
+            tabsContent.classList.remove("active-tab")
+        })
+        target.classList.add("active-tab")
+
+        tabs.forEach((tab) => {
+            tab.classList.remove("active-tab")
+        })
+        tab.classList.add("active-tab")
+    })
+})
