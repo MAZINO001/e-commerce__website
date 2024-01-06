f75b73bda4d59c7d34be8fa1511cb3ea077ef695
mounir <marnissimounir05@gmail.com>
2024-01-06 15:04:30 +0100

    finish project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,22 @@
 /*=============== SHOW MENU ===============*/
+const NavMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById("nav-toggle")
+navClose = document.getElementById("nav-close");
 
 /*===== Menu Show =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        NavMenu.classList.add("show-menu")
+    })
+}
 /*===== Hide Show =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        NavMenu.classList.remove("show-menu")
+    })
+}
 /*=============== IMAGE GALLERY ===============*/
 function imgGallery() {
     const mainImg = document.querySelector(".details__img"),
